4eae0219e505605311a8827cd2d91d83986ade70
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-06-13 18:00:52 +0900

    define a ultimate dark macro

--- src/functions.rs
+++ src/functions.rs
@@ -4,58 +4,66 @@ use std::os::raw::c_char;
 use const_variables::*;
 use types::*;
 
+//極大闇魔黒呪文
+macro_rules!  dx_func{
+    ($fname:ident($($argname:ident;$argt:ty),*) -> $r:ty;wrap ($($w_argname:ident;$w_argt:ty),*)->$w_r:ty {$($st:stmt);*}$($tt:tt)*) => {
+        mod $fname{
+            use std::os::raw::c_char;
+            use const_variables::*;
+            use types::*;
+            #[link(name = "DxLib_x64")]
+            #[no_mangle]
+            extern "stdcall" {
+                pub fn $fname($($argname:$argt),* )->$r;
+            }
+        }
+        pub fn $fname($($w_argname:$w_argt),* )->$w_r {
+            use self::$fname::$fname as this;
+            $($st)*
+        }
+        dx_func!{$($tt)*}
+    };
+    ($fname:ident($($argname:ident;$argt:ty),*) -> $r:ty; $($tt:tt)*) => {
+        #[link(name = "DxLib_x64")]
+        #[no_mangle]
+        extern "stdcall" {
+            pub fn $fname($($argname:$argt),* )->$r;
+        }
+        dx_func!($($tt)*);        
+    };
+    ()=>{};
+}
+
 /*dxlib function extern declaration*/
 
-#[link(name = "DxLib_x64")]
-#[no_mangle]
-extern "stdcall" {
-
+dx_func!{
     //使用必須関数
-    pub fn dx_DxLib_Init() -> i32;
-    pub fn dx_DxLib_End() -> i32;
-    pub fn dx_ProcessMessage() -> i32;
-
+    dx_DxLib_Init()->i32;
+    dx_DxLib_End() -> i32;
+    dx_ProcessMessage() -> i32;
     //図形描画関数
-    pub fn dx_DrawGraph(x: i32, y: i32, GrHandle: i32, TransFlag: i32) -> i32;
-
-    //その他画面操作系関数
-    pub fn dx_SetGraphMode(SizeX: i32, SizeY: i32, ColorBitNum: i32, RefreshRate: i32) -> i32;
-    pub fn dx_SetDrawScreen(DrawScreen: i32) -> i32;
-    pub fn dx_ScreenFlip() -> i32;
-
-    //ウエイト関係の関数
-    pub fn dx_WaitKey() -> i32;
-
-    //ウインドウモード関係
-    pub fn dx_ChangeWindowMode(flag: i32) -> i32;
-    pub fn dx_SetMainWindowText(WindowText: *const u8) -> i32;
-
-}
-
-/*wrapped function*/
-mod hidden {
-    use std::os::raw::c_char;
-    use types::*;
-    #[link(name = "DxLib_x64")]
-    #[no_mangle]
-    extern "stdcall" {
-        pub fn dx_ClearDrawScreen(ClearRect: *mut RECT) -> i32;
-        pub fn dx_LoadGraph(FileName: *const c_char, NotUse3DFlag: i32) -> i32;
-
+    dx_LoadGraph(FileName; *const c_char, NotUse3DFlag; i32) -> i32;
+    wrap(FileName;&str)->i32 {
+        unsafe { this(CString::new(FileName).unwrap().as_ptr(), FALSE) }
     }
-}
-
-/*wrap function*/
-pub fn dx_ClearDrawScreen() -> i32 {
-    let mut tmp = RECT {
-        left: -1,
-        right: -1,
-        top: -1,
-        bottom: -1,
-    };
-    unsafe { hidden::dx_ClearDrawScreen(&mut tmp) }
-}
-
-pub fn dx_LoadGraph(FileName: &str) -> i32 {
-    unsafe { hidden::dx_LoadGraph(CString::new(FileName).unwrap().as_ptr(), FALSE) }
+    dx_DrawGraph(x; i32, y; i32, GrHandle; i32, TransFlag; i32) -> i32;
+    //その他画面操作系関数
+    dx_SetGraphMode(SizeX; i32, SizeY; i32, ColorBitNum; i32, RefreshRate; i32) -> i32;
+    dx_SetDrawScreen(DrawScreen; i32) -> i32;
+    dx_ScreenFlip() -> i32;
+    dx_ClearDrawScreen(ClearRect; *mut RECT)->i32;
+    wrap()->i32 {
+         let mut tmp = RECT {
+            left: -1,
+            right: -1,
+            top: -1,
+            bottom: -1,
+        };
+        unsafe { this(&mut tmp) }
+    }
+    //ウエイト関係の関数
+    dx_WaitKey() -> i32;
+    //ウインドウモード関係
+    dx_ChangeWindowMode(flag; i32) -> i32;
+    dx_SetMainWindowText(WindowText; *const u8) -> i32;
 }
