2d1c56b26d70a8d3d03be4a2cdf693c7e22ad091
パン粉 ライ麦 <relipmoc101@gmail.com>
2018-06-14 22:19:37 +0900

    fix macro and add dx_function

---
+++ examples/draw.rs
@@ -0,0 +1,44 @@
+extern crate rust_dxlib;
+use rust_dxlib::*;
+
+fn main() {
+    game();
+}
+
+fn game() {
+    unsafe {
+        //ウィンドウモード変更と初期化と裏画面設定
+        dx_ChangeWindowMode(TRUE);
+        dx_DxLib_Init();
+        dx_SetDrawScreen(DX_SCREEN_BACK);
+
+        // while( 裏画面を表画面に反映, メッセージ処理, 画面クリア )
+        while (dx_ScreenFlip() == 0 && dx_ProcessMessage() == 0 && dx_ClearDrawScreen() == 0) {
+            /*dx_DrawLine(10, 10, 500, 500, dx_GetColor(255, 255, 255), 10);
+            dx_DrawLineAA(100.3, 10.4, 500.550, 500.4, dx_GetColor(255, 255, 255), 2.6);
+            dx_DrawBox(80, 80, 200, 150, dx_GetColor(255, 200, 255), FALSE);
+            dx_DrawBox(200, 200, 250, 300, dx_GetColor(0, 255, 255), TRUE);*/
+            dx_DrawBoxAA(
+                100.0,
+                30.0,
+                200.0,
+                30.0,
+                dx_GetColor(0, 255, 255),
+                TRUE,
+                1.0,
+            );
+            dx_DrawCircle(150, 150, 20, dx_GetColor(255, 255, 0), TRUE, 5);
+            dx_DrawCircleAA(
+                120.3,
+                100.2,
+                20.0,
+                100,
+                dx_GetColor(255, 255, 0),
+                FALSE,
+                5.0,
+            );
+        }
+
+        dx_DxLib_End(); // DXライブラリ終了処理
+    }
+}
--- src/functions.rs
+++ src/functions.rs
@@ -6,7 +6,7 @@ use types::*;
 
 //極大闇魔黒呪文
 macro_rules!  dx_func{
-    ($fname:ident($($argname:ident;$argt:ty),*) -> $r:ty;wrap ($($w_argname:ident;$w_argt:ty),*)->$w_r:ty {$($st:stmt);*}$($tt:tt)*) => {
+    ($fname:ident($($argname:ident:$argt:ty),*) -> $r:ty;wrap ($($w_argname:ident:$w_argt:ty),*)->$w_r:ty {$($st:stmt);*}$($tt:tt)*) => {
         mod $fname{
             use std::os::raw::c_char;
             use const_variables::*;
@@ -23,7 +23,7 @@ macro_rules!  dx_func{
         }
         dx_func!{$($tt)*}
     };
-    ($fname:ident($($argname:ident;$argt:ty),*) -> $r:ty; $($tt:tt)*) => {
+    ($fname:ident($($argname:ident:$argt:ty),*) -> $r:ty; $($tt:tt)*) => {
         #[link(name = "DxLib_x64")]
         #[no_mangle]
         extern "stdcall" {
@@ -42,16 +42,21 @@ dx_func!{
     dx_DxLib_End() -> i32;
     dx_ProcessMessage() -> i32;
     //図形描画関数
-    dx_LoadGraph(FileName; *const c_char, NotUse3DFlag; i32) -> i32;
-    wrap(FileName;&str)->i32 {
+    dx_DrawLine(x1:i32 ,y1:i32 ,x2:i32 ,y2:i32 , Color:u32 ,Thickness:i32)->i32 ;
+    dx_DrawLineAA(x1:f32, y1:f32, x2:f32, y2:f32, Color:u32, Thickness:f32)->i32;
+    dx_DrawBox(x1:i32, y1:i32, x2:i32, y2:i32, Color:u32, FillFlag:i32)->i32;
+    dx_DrawBoxAA(x1:f32,y1:f32,x2:f32, y2:f32, Color:u32, FillFlag:i32, LineThickness:f32)->i32;
+    dx_DrawCircle(x:i32,y:i32,r:i32,Color:u32,FillFlag:i32, LineThickness:i32)->i32;
+    dx_DrawCircleAA(x:f32, y:f32, r:f32,posnum:i32,Color:u32, FillFlag:i32,LineThickness:f32)->i32;
+
+    dx_LoadGraph(FileName: *const c_char, NotUse3DFlag: i32) -> i32;
+    wrap(FileName:&str)->i32 {
         unsafe { this(CString::new(FileName).unwrap().as_ptr(), FALSE) }
     }
-    dx_DrawGraph(x; i32, y; i32, GrHandle; i32, TransFlag; i32) -> i32;
+    dx_DrawGraph(x: i32, y: i32, GrHandle: i32, TransFlag: i32) -> i32;
     //その他画面操作系関数
-    dx_SetGraphMode(SizeX; i32, SizeY; i32, ColorBitNum; i32, RefreshRate; i32) -> i32;
-    dx_SetDrawScreen(DrawScreen; i32) -> i32;
-    dx_ScreenFlip() -> i32;
-    dx_ClearDrawScreen(ClearRect; *mut RECT)->i32;
+    dx_SetGraphMode(SizeX: i32, SizeY: i32, ColorBitNum: i32, RefreshRate: i32) -> i32;
+    dx_ClearDrawScreen(ClearRect: *mut RECT)->i32;
     wrap()->i32 {
          let mut tmp = RECT {
             left: -1,
@@ -61,9 +66,14 @@ dx_func!{
         };
         unsafe { this(&mut tmp) }
     }
+    dx_GetColor(Red:i32,Green:i32 ,Blue:i32)->u32;
+    dx_SetDrawScreen(DrawScreen: i32) -> i32;
+    dx_ScreenFlip() -> i32;
+
+
     //ウエイト関係の関数
     dx_WaitKey() -> i32;
     //ウインドウモード関係
-    dx_ChangeWindowMode(flag; i32) -> i32;
-    dx_SetMainWindowText(WindowText; *const u8) -> i32;
+    dx_ChangeWindowMode(flag: i32) -> i32;
+    dx_SetMainWindowText(WindowText: *const u8) -> i32;
 }
